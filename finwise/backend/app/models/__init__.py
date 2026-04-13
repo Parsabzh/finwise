0f8f3f6842baab4c36a7fb3a8a7fe860ba02efe8
parsabzh <parsabzh@gmail.com>
2026-04-13 17:57:20 +0200

    fix git

--- finwise/backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.user import User
-from app.models.transaction import Transaction
-from app.models.budget import Budget
-from app.models.saving_goals import SavingGoals
-from app.models.recurring_transaction import RecurringTransaction
