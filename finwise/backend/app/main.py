069d1c38f9b0b1ff50029c6a67aa905196805d7c
parsabzh <parsabzh@gmail.com>
2026-04-17 13:30:07 +0200

    add frontend url

--- finwise/backend/app/main.py
+++ finwise/backend/app/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",    # Next.js dev server
         "http://127.0.0.1:3000",
+        "https://finwise-frontend-delta.vercel.app" #prod
     ],
     allow_credentials=True,
     allow_methods=["*"],
